5c26dae9a7e937d4a79d0a2947297dd52434e619
rasec1106 <elch.herrera@gmail.com>
2023-09-15 19:57:46 -0500

    Create serial class

--- proyectoSerializacion/src/MyClasses/Person.java
+++ proyectoSerializacion/src/MyClasses/Person.java
@@ -1,5 +1,50 @@
 package MyClasses;
 
-public class Person {
+import java.io.Serializable;
 
+public class Person implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	// Attributes
+	private String dni;
+	private String name;
+	private int age;
+	
+	// Constructors
+	public Person() {
+		super();
+	}
+
+	public Person(String dni, String name, int age) {
+		super();
+		this.dni = dni;
+		this.name = name;
+		this.age = age;
+	}
+
+	// Getters and Setters
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
---
+++ proyectoSerializacion/src/MyClasses/Serial1.java
@@ -0,0 +1,47 @@
+package MyClasses;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class Serial1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p;
+		FileOutputStream fos = null;
+		ObjectOutputStream out = null;
+		
+		try {
+			// Create file
+			fos = new FileOutputStream("person.dat");
+			out = new ObjectOutputStream(fos);
+			
+			// Create person object 1
+			p = new Person("12345678", "Jose Lopez", 35);
+			out.writeObject(p);
+			
+			// Create person object 2
+			p = new Person("98467509", "Flor Becerra", 31);
+			out.writeObject(p);
+			
+			// Create person object 3
+			p = new Person("66725642", "Luis Suarez", 27);
+			out.writeObject(p);
+			
+		}catch (FileNotFoundException e) {
+			System.out.println("1 - "+e.getMessage());
+		}catch (IOException e) {
+			System.out.println("2 - "+e.getMessage());
+		}finally {
+			try {
+				if(fos != null) fos.close();
+				if(out != null) out.close();
+			}catch (IOException e) {
+				System.out.println("3 - "+e.getMessage());
+			}
+		}
+	}
+
+}
